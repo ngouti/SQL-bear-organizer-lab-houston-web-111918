f4905aef536b8b60d852bba8abe66a1f722209cc
github username <github email address>
2018-11-28 23:47:48 -0600

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY, --> has to be capital 
+  name text,
+  age integer,
+  gender text,
+  color text,
+  temperament text,
+  alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 8, 'M', 'brown', 'calm', 1),  
+    ('Rowdy', 3, 'M', 'black', 'energetic', 1),   
+    ('Tabitha', 20, 'F', 'black', 'anxious', 0),  
+    ('Sergeant Brown', 15, 'F', 'brown', 'aggressive', 1),  
+    ('Melissa', 9, 'F', 'brown', 'calm', 1),  
+    ('Grinch', 18, 'M', 'black', 'stubborn', 0),  
+    ('Wendy', 2, 'M', 'black', 'curious', 1),
+    (NULL, 7, 'F', 'black', 'angry', 1);
+    --> 1 or 0 for true or false
